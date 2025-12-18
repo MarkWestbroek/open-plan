a6e92a3f1d6f060e844ae6901aa70be1f1060f8f
Tim de Beer <tim@maykinmedia.nl>
2025-12-18 09:43:40 +0100

    :sparkles: [#3] setup file structure

---
+++ src/openplan/plannen/api/urls.py
@@ -0,0 +1,42 @@
+from django.urls import include, path
+
+from drf_spectacular.views import (
+    SpectacularRedocView,
+)
+from vng_api_common import routers
+
+from ...utils.views import SpectacularJSONAPIView, SpectacularYAMLAPIView
+from .schema import custom_settings
+
+app_name = "plannen_api"
+
+router = routers.DefaultRouter()
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("", router.APIRootView.as_view(), name="root"),
+    path(
+        "openapi.json",
+        SpectacularJSONAPIView.as_view(
+            urlconf="openplan.plannen.api.urls",
+            custom_settings=custom_settings,
+        ),
+        name="schema-json-plannen",
+    ),
+    path(
+        "openapi.yaml",
+        SpectacularYAMLAPIView.as_view(
+            urlconf="openplan.plannen.api.urls",
+            custom_settings=custom_settings,
+        ),
+        name="schema-yaml-plannen",
+    ),
+    path(
+        "schema/",
+        SpectacularRedocView.as_view(
+            url_name="plannen_api:schema-yaml-plannen",
+        ),
+        name="schema-redoc-plannen",
+    ),
+]
