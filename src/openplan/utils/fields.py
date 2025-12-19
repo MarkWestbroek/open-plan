4e67246b9c431de5256dbf76f4b57b35f674562a
Tim de Beer <tim@maykinmedia.nl>
2025-12-19 14:27:07 +0100

    :sparkles: [#4] add urn utils

--- src/openplan/utils/fields.py
+++ src/openplan/utils/fields.py
@@ -1,10 +1,27 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.db import models
 from django.utils.encoding import smart_str
 from django.utils.translation import gettext_lazy as _
 
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
+
+from .validators import URIValidator
+
+
+class URIField(models.CharField):
+    """
+    A Django model field for Uniform Resource Identifiers (RFC 3986),
+    supporting both URLs and URNs.
+    """
+
+    default_validators = [URIValidator()]
+    description = _("URI")
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("max_length", 500)
+        super().__init__(*args, **kwargs)
 
 
 @extend_schema_field(OpenApiTypes.UUID)
