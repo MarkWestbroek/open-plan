c3f9d87e1e6e9f539fafa242a88c0406586758b1
Tim de Beer <tim@maykinmedia.nl>
2026-02-11 15:59:24 +0100

    :sparkles: [#26] add plan and doel filters

--- src/openplan/plannen/api/filtersets/doel.py
+++ src/openplan/plannen/api/filtersets/doel.py
@@ -3,19 +3,37 @@ from django.utils.translation import gettext_lazy as _
 from vng_api_common.filtersets import FilterSet
 
 from openplan.plannen.models.doel import Doel
-from openplan.utils.filters import (
-    UUIDFInFilter,
-)
+from openplan.utils.filters import UUIDFInFilter
 
 
 class DoelFilter(FilterSet):
     doeltype_uuid = UUIDFInFilter(
         field_name="doeltype__uuid",
-        lookup_expr="in",
         distinct=True,
         help_text=_("UUID's van de gekoppelde doeltypen."),
+    )
+    persoon_uuid = UUIDFInFilter(
+        field_name="persoon__uuid",
+        distinct=True,
+        help_text=_("UUID's van de gekoppelde personen."),
+    )
+    plannen_uuids = UUIDFInFilter(
+        field_name="plannen__uuid",
+        distinct=True,
+        help_text=_("UUID's van de gekoppelde plannen."),
+    )
+    hoofd_doel_uuid = UUIDFInFilter(
+        field_name="hoofd_doel__uuid",
+        distinct=True,
+        help_text=_("UUID's van de bovenliggende doelen."),
     )
 
     class Meta:
         model = Doel
-        fields = {}
+        fields = {
+            "status": ["exact", "in"],
+            "titel": ["exact", "icontains"],
+            "startdatum": ["exact", "gte", "lte"],
+            "einddatum": ["exact", "gte", "lte"],
+            "resultaat": ["exact", "icontains"],
+        }
--- src/openplan/plannen/api/filtersets/plan.py
+++ src/openplan/plannen/api/filtersets/plan.py
@@ -3,19 +3,30 @@ from django.utils.translation import gettext_lazy as _
 from vng_api_common.filtersets import FilterSet
 
 from openplan.plannen.models.plan import Plan
-from openplan.utils.filters import (
-    UUIDFInFilter,
-)
+from openplan.utils.filters import UUIDFInFilter
 
 
 class PlanFilter(FilterSet):
     plantype_uuid = UUIDFInFilter(
         field_name="plantype__uuid",
-        lookup_expr="in",
         distinct=True,
         help_text=_("UUID's van de gekoppelde plantypen."),
+    )
+    overkoepelend_plan_uuid = UUIDFInFilter(
+        field_name="overkoepelend_plan__uuid",
+        distinct=True,
+        help_text=_("UUID van het overkoepelend plan."),
     )
 
     class Meta:
         model = Plan
-        fields = {}
+        fields = {
+            "status": ["exact"],
+            "fase": ["exact"],
+            "titel": ["exact", "icontains"],
+            "medewerker": ["exact"],
+            "zaak": ["exact"],
+            "domeinregister": ["exact"],
+            "startdatum": ["exact", "gte", "lte"],
+            "einddatum": ["exact", "gte", "lte"],
+        }
